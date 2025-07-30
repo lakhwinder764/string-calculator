8afdd1e635ffe6bd24102269245d28bc14910803
lakhwinder764 <lakhwinder10119@gmail.com>
2025-07-30 20:13:57 +0530

    throws error on negative numbers

--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -17,3 +17,6 @@ test("supports newlines as separators", () => {
 test("supports custom delimiter", () => {
   expect(add("//;\n1;2")).toBe(3);
 });
+test("throws error on negative numbers", () => {
+  expect(() => add("1,-2,3,-4")).toThrow("negative numbers not allowed -2,-4");
+});
