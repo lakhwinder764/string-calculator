c2390897beb4128e102401746d79475b4d8e595c
lakhwinder764 <lakhwinder10119@gmail.com>
2025-07-30 20:04:31 +0530

    returns sum for two or more numbers

--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -7,3 +7,6 @@ test("returns 0 for empty string", () => {
 test("returns the number itself if only one number", () => {
   expect(add("4")).toBe(4);
 });
+test("returns sum for two or more numbers", () => {
+  expect(add("1,2,6,7,5,67,4,6,4,6")).toBe(108);
+});
